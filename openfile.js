3de6a5733f25decd72807831d368730ee7583abf
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-07-02 23:15:56 +0530

    first commit

---
+++ openfile.js
@@ -0,0 +1,7 @@
+// fs.open(path, flag, [mode], callback)
+// fs.openSync(path, flag, [mode]) -> synchronous
+
+const dirfile = "dir.txt";
+const flagsfile = "flags.txt";
+
+const fs = require("fs");
